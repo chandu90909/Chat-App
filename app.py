2869093d1fe7b0f655cbd2cc8a133b74b0840ab8
unknown <chalapathikommuru@gmail.com>
2026-03-27 19:15:09 +0530

    final stable deploy fix

--- app.py
+++ app.py
@@ -1,84 +1,21 @@
-from flask import Flask, render_template, request
-from flask_socketio import SocketIO, send, join_room, emit
-import os
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
 
-# IMPORTANT: threading mode (no eventlet)
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode='threading')
-
-rooms = {}
-clients = {}
-messages = {}   # in-memory messages
-
+messages = []
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', messages=messages)
 
+@app.route('/send', methods=['POST'])
+def send():
+    username = request.form['username']
+    message = request.form['message']
 
-# JOIN ROOM
-@socketio.on('join')
-def on_join(data):
-    username = data['user']
-    room = data['room']
-    sid = request.sid
+    messages.append(f"{username}: {message}")
 
-    join_room(room)
+    return redirect('/')
 
-    # store client
-    clients[sid] = {"user": username, "room": room}
-
-    # add to room
-    if room not in rooms:
-        rooms[room] = []
-    if username not in rooms[room]:
-        rooms[room].append(username)
-
-    # send user list
-    emit('user_list', rooms[room], to=room)
-
-    # send old messages
-    if room in messages:
-        for msg in messages[room]:
-            emit('message', msg)
-
-    send(f"{username} joined {room}", to=room)
-
-
-# SEND MESSAGE
-@socketio.on('message')
-def handle_message(data):
-    room = data['room']
-
-    # store messages
-    if room not in messages:
-        messages[room] = []
-    messages[room].append(data)
-
-    send(data, to=room)
-
-
-# DISCONNECT
-@socketio.on('disconnect')
-def on_disconnect():
-    sid = request.sid
-
-    if sid in clients:
-        user = clients[sid]['user']
-        room = clients[sid]['room']
-
-        if room in rooms and user in rooms[room]:
-            rooms[room].remove(user)
-
-        emit('user_list', rooms.get(room, []), to=room)
-        send(f"{user} left the room", to=room)
-
-        del clients[sid]
-
-
-# RUN APP (Railway compatible)
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=5000)
